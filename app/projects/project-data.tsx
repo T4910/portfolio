cb92a292494bc0fbdb4f87553ebda1f0a7d6027d
T4910 <ttaiwo4910@gmail.com>
2025-04-14 17:27:28 +0100

    ✨new projects

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -17,7 +17,7 @@ export const projects: Project[] = [
     title: "NextAuth Authentication with Nextjs",
     year: 2024,
     description:
-    "A project I used to learn how to use NextAuth for authentication & authorization in Nextjs",
+      "A project I used to learn how to use NextAuth for authentication & authorization in Nextjs",
     url: "https://nextjs-auth-fawn.vercel.app/",
   },
   {
@@ -30,22 +30,34 @@ export const projects: Project[] = [
   {
     title: "TradeUp",
     year: 2022,
-    description:
-      "Literally just a fun little project when I started web dev",
+    description: "Literally just a fun little project when I started web dev",
     url: "https://trading-up.vercel.app/",
   },
   {
     title: "Landmark University Evangelism website",
     year: 2023,
-    description:
-      "A showcase website for Landmark Univeristy Evangelism Unit",
+    description: "A showcase website for Landmark Univeristy Evangelism Unit",
     url: "https://lueg.vercel.app/",
   },
   {
     title: "GHM website",
     year: 2025,
     description:
-      "A website for showcasing GHM & contain critical software for managing GHM activities",
-    url: "https://ghm.lmu.edu.ng",
+      "A website for showcasing GHM & contain critical software for managing GHM activities (under development)",
+    url: "https://ghm-ashen.vercel.app/",
+  },
+  {
+    title: "Oxygen loaning platform",
+    year: 2025,
+    description:
+      "A loaning platform for students to borrow money from their peers and pay back with interest. This was a project that was given to me for a job interview. I had to build this project using Angular, a framework I had never used before. That was a fun experience.",
+    url: "https://ng-oxygen.vercel.app/",
+  },
+  {
+    title: "Task Managment Platform",
+    year: 2025,
+    description:
+      "A task management platform for managing tasks and projects. This was a project that was given to me for a job interview. I used Nextjs for the frontend, deployed on vercel and and Hono.js for the backend deployed on Cloudflare Workers.",
+    url: "https://social-inbox-frontend.vercel.app/",
   },
 ];
